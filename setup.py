1fb562aa7055914e73d4f038609daf4406fd16e9
Çağdaş <cagdas.oztekin@mobilearts.com>
2019-12-05 10:15:55 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
   version = '0.18',
   scripts=['bin/ml_easy_peer_grade'],
   description = 'Ez peer grade your project members, exclusive to privileged Bilkent students',
-  author = 'Cagdas Oztekin',
+  author = 'Cuklahan Dorum',
   author_email = 'badass@alumni.bilkent.edu.tr',
   url = 'https://github.com/cagdass/ml-easy-peer-grade', 
   download_url = 'https://github.com/cagdass/ml-easy-peer-grade/tarball/0.1',
